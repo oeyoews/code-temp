bdca13b049234f6f53eab58e1ffecbe59e4b53df
oeyoews <jyao4783@gmail.com>
2023-12-30 16:40:12 +0800

    add plum

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function App() {
   return <h1 class="text-3xl font-bold p-4">Hello, World!</h1>;
 }
 createRoot(globalThis.root).render(<App />);
+
+const d = require('ejs.min.js');
+const np = require('nprogress.min.js');
+const n = require('simple-notify.min.js');
